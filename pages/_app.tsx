4fca2f75eccebaf25f9618faac1bddd8bf09d362
Dmitriy Malakhov <mitia2022@gmail.com>
2022-06-03 15:32:41 +0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import { AppProps } from "next/app";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import { Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Container, Flex, Heading, useColorMode } from '@chakra-ui/react';
 
 export default function IndexPage() {
+  const { toggleColorMode } = useColorMode();
   return (
-    <>
+    <Container maxW={'7xl'} px={[4, null, 16]}>
       <Heading>Welcome to Chakra UI</Heading>
-      <Button colorScheme="telegram">Get started</Button>
-    </>
+
+      <Button
+        colorScheme="facebook"
+        onClick={() => {
+          toggleColorMode();
+        }}
+      >
+        Change theme
+      </Button>
+    </Container>
   );
 }
